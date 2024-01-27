89d9c020de6adc9dd25fcf3b5463e10c7415a621
anabb55 <75089113+anabb55@users.noreply.github.com>
2024-01-27 22:39:38 +0100

    Project setup

---
+++ position-simulator/src/main/java/com/ISAPROJEKAT/positionsimulator/PositionSimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.ISAPROJEKAT.positionsimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PositionSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PositionSimulatorApplication.class, args);
+	}
+
+}
