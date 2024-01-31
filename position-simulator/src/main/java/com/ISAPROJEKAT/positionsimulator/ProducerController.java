db5debfaa4d1bd030b9cee5c34b770b595798fc2
anabb55 <75089113+anabb55@users.noreply.github.com>
2024-01-31 19:42:36 +0100

    cors

--- position-simulator/src/main/java/com/ISAPROJEKAT/positionsimulator/ProducerController.java
+++ position-simulator/src/main/java/com/ISAPROJEKAT/positionsimulator/ProducerController.java
@@ -2,11 +2,7 @@ package com.ISAPROJEKAT.positionsimulator;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.util.List;
@@ -24,6 +20,7 @@ public class ProducerController {
         return ResponseEntity.ok().build();
     }
 
+    @CrossOrigin(origins = "*")
     @PostMapping(value="/{exchange}/{queue}")
     public ResponseEntity<String> sendMessageToExchange(@PathVariable("exchange") String exchange, @PathVariable("queue") String queue, @RequestBody List<Location> StartEndPosition) throws IOException, InterruptedException {
         producer.sendToExchange(exchange, queue, StartEndPosition);
